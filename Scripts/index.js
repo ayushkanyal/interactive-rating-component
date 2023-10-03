f4c7d08070d18eb75724570cedd2cc86ceb1b833
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-03 15:35:48 +0530

    Update index.js
    Added updateScore() function to fetch optio values to thank you screen.

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,18 +1,21 @@
-const scores = document.getElementsByClassName('rating__score');
+const scores = document.getElementsByClassName("rating__score");
 
 for (let score of scores) {
-    score.addEventListener("click", handleClick);
+  score.addEventListener("click", handleClick);
 }
 
-function handleClick(event){    
-    const elementId = event.target.id;
-    document.getElementById(elementId).classList.add("selected");
+function handleClick(event) {
+  const elementId = event.target.id;
+  document.getElementById(elementId).classList.add("selected");
+  updateScore(event.target.id);
 }
 
-function handleSubmit(event){
-   document.getElementsByClassName("feedback")[0].classList.add("hidden");
-   document.getElementsByClassName("thank--you")[0].classList.remove("hidden");
-   let score = document.getElementsByClassName("thank--you__score")[0]; 
-   console.log(event);
-   console.log(score.innerHTML);
+function updateScore(score) {
+  let scoreText = document.getElementsByClassName("thank--you__score")[0];
+  scoreText.innerHTML = `You selected ${score} out of 5`;
+}
+
+function handleSubmit(event) {
+  document.getElementsByClassName("feedback")[0].classList.add("hidden");
+  document.getElementsByClassName("thank--you")[0].classList.remove("hidden");
 }
