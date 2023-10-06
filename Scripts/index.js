2bfbf4aefa1986ef68d61ac32710323d4a171485
Ayush <114506088+ayushkanyal@users.noreply.github.com>
2023-10-07 01:01:04 +0530

    Update index.js
    Added refreshlist() logic so that only one button can be selected at a time. 
    Added if condition to check if an option has been selected,otherwise form is not submitted.

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,9 +4,26 @@ for (let score of scores) {
   score.addEventListener("click", handleClick);
 }
 
+function refreshList() {
+  const selectedButton = document.querySelector(".selected");
+  console.log(selectedButton);
+  if (selectedButton) {
+    selectedButton.classList.remove("selected");
+  }
+}
+
+refreshList();
+
 function handleClick(event) {
   const elementId = event.target.id;
-  document.getElementById(elementId).classList.add("selected");
+  const selectedItem = document.getElementById(elementId);
+  console.log(elementId);
+  if (selectedItem.classList.contains("selected")) {
+    selectedItem.classList.remove("selected");
+  } else {
+    refreshList();
+    selectedItem.classList.add("selected");
+  }
   updateScore(event.target.id);
 }
 
@@ -15,7 +32,10 @@ function updateScore(score) {
   scoreText.innerHTML = `You selected ${score} out of 5`;
 }
 
-function handleSubmit(event) {
-  document.getElementsByClassName("feedback")[0].classList.add("hidden");
-  document.getElementsByClassName("thank--you")[0].classList.remove("hidden");
+function handleSubmit() {
+  const selectedButton = document.querySelector(".selected");
+  if (selectedButton) {
+    document.getElementsByClassName("feedback")[0].classList.add("hidden");
+    document.getElementsByClassName("thank--you")[0].classList.remove("hidden");
+  }
 }
